ea3048bd6a5093fc4cba9b25c19c6a9e3b1a8f2b
hisham837 <mhdhisham.837@gmail.com>
2025-06-09 17:10:39 +0530

    Your Message

---
+++ instructions to read.R
@@ -0,0 +1,9 @@
+#instructions
+#First create a pdf inside pdf creator folder using yml from jss template file
+#keep the pdf in docs
+# add the download button code in the starting of qmd
+# ::: {.download-button}
+# <a href="jsstemplate.pdf" class="btn btn-primary" download>
+#   <i class="bi bi-file-pdf"></i> Download PDF
+#   </a>
+#     :::  
